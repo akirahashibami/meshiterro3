e38b4b8461647245914ca13125fd81c2be4055a6
akirahashibami <hashibami2207@gmail.com>
2020-07-17 10:07:31 +0000

    [Finish] 5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '29fe55cd95817b96b07d1b12f5f1f43d903b7bdedfb9922a8ba8ee1b77bebae926b2567193ac379b4d21897c271bdd2d387eb480e09105f2f134754293a077fd'
